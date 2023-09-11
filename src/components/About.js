9f9a351201b831cbaed7967416dc3c74f9366862
rodrigueness7 <alex.correarodrigues@hotmail.com>
2023-09-11 08:50:23 -0300

    modifying about and home logo

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,10 @@ import React from "react"
 function About(){
     return(
        <section id="about">
-        <div>
-         <h1>Sobre</h1>
-            <p1 className="col-sm-6">Transforme sua unhas em obras de arte:
+        <div className="sobre">
+         <br/><br/>
+         <h1 className="d-flex justify-content-center">Sobre</h1>
+            <p1 className="text-sm-left">Transforme sua unhas em obras de arte:
              O segredo da elegância e estilo inigualáveis! Descubra um mundo
              de possibilidade com nossos produtos exclusivos para unhas. pronta
              para arrasar em qualquer ocasião com unhas deslumbrante e criativa.
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,6 @@ import React from "react"
           <div className="container d-flex flex-column justify-content-center"> 
           <img className="img-fluid logo"  alt="logo" src="/assets/logo.png"></img>
           <div className="d-flex flex-column">
-            <h1 className="col-sm-6 text-white">BELEZZADELA COSMETICS</h1>
             <div>
               <a href="https://instagram.com"><i className="bi bi-instagram icon-social"></i></a>
               <a href="https://web.whatsapp.com/"><i className="bi bi-whatsapp icon-social"></i></a>
