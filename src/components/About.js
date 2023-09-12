2099ccc7456916df674db2376db10ff7d19a888e
Rodrigueness7 <104371197+Rodrigueness7@users.noreply.github.com>
2023-09-12 20:43:52 -0300

    changing about

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,19 @@ import React from "react"
 function About(){
     return(
        <section id="about">
-        <div className="sobre">
+        <div>
          <br/><br/>
-         <h1 className="d-flex justify-content-center">Sobre</h1>
-            <p1 className="text-sm-left">Transforme sua unhas em obras de arte:
+         <div className="about-container">
+         <h1 className="d-flex justify-content-center ">Sobre</h1>
+            <div className="width border text d-flex align-items-center">Transforme sua unhas em obras de arte:
              O segredo da elegância e estilo inigualáveis! Descubra um mundo
              de possibilidade com nossos produtos exclusivos para unhas. pronta
              para arrasar em qualquer ocasião com unhas deslumbrante e criativa.
              Não espere, deixe suas unhas contarem sua história de beleza. 
-            </p1>
-       </div>    
+            </div>
+         </div>
+         </div>
+
        </section>
     )
  }
