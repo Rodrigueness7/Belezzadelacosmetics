69329cc8a58b36617ae58192e1550cd04b6bea33
rodrigueness7 <alex.correarodrigues@hotmail.com>
2023-09-13 08:51:15 -0300

    Fixing page about.

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,10 @@ function About(){
        <section id="about">
         <div>
          <br/><br/>
-         <div className="about-container">
-         <h1 className="d-flex justify-content-center ">Sobre</h1>
-            <div className="width border text d-flex align-items-center">Transforme sua unhas em obras de arte:
+         <div className="d-flex flex-column border width-border ">
+         <h1 className="d-flex justify-content-center">Sobre</h1>
+            <div className="text width-title border">
+              Transforme sua unhas em obras de arte:
              O segredo da elegância e estilo inigualáveis! Descubra um mundo
              de possibilidade com nossos produtos exclusivos para unhas. pronta
              para arrasar em qualquer ocasião com unhas deslumbrante e criativa.
